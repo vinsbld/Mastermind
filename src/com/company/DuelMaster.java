3cddaf1fe72df7fe085a591795f86f2ee8f0730a
vinsbld <vincent.blndel@gmail.com>
2019-04-08 10:20:40 +0200

    Gestion des exceptions pour le mode Duel, Mastermind/Recherche

--- src/com/company/DuelMaster.java
+++ src/com/company/DuelMaster.java
@@ -16,96 +16,191 @@ public class DuelMaster {
         for (int i = 0; i < tabNbSecretCpu.length; i++) {
             tabNbSecretCpu[i] = nbAleatoire.nextInt(9);
         }
-        System.out.print(Arrays.toString(tabNbSecretCpu));
-        System.out.println();
 
         System.out.println("SAISISSEZ VOTRE CHIFFRE MYSTERE : ");
         String nbSecretUtilisateur = nb.next();
+
+        /*tant que la saisiUtilisateur n'est pas un nombre entre 0 et 9
+         envoie un message d'erreur*/
+
+        boolean isUnNombre = nbSecretUtilisateur.matches("[0-9]*");
+
+        while (!isUnNombre) {
+            Utils.etoileDecorationPourMaster();
+            System.out.println("Vous n'avez pas saisi un nombre !");
+            Utils.etoileDecorationPourMaster();
+            System.out.println("SAISISSEZ VOTRE CHIFFRE MYSTERE : ");
+            nbSecretUtilisateur = nb.next();
+            isUnNombre = nbSecretUtilisateur.matches("[0-9]*");
+        }
+
         int[] tabNbSecretUtil = new int[longueurDelaCombinaison];
         for (int j = 0; j < tabNbSecretUtil.length; j++) {
             int converter = Integer.parseInt(nbSecretUtilisateur.charAt(j) + "");
             tabNbSecretUtil[j] = converter;
         }
-        Utils.etoileDecorationPourMaster();
-        System.out.println("Votre code secret est : " + Arrays.toString(tabNbSecretUtil));
-        Utils.etoileDecorationPourMaster();
-        System.out.println();
 
+        /*tant que la proposition n'est pas de la bonne taille envoie ce message d'erreur,
+        y++ fait en sorte que chaque mauvaise saisie n'est pas compté comme un essai*/
 
-        for (int i = nbEssai; i >= 0; i--) {
+        while (nbSecretUtilisateur.length() < tabNbSecretCpu.length || nbSecretUtilisateur.length() > tabNbSecretCpu.length) {
+            Utils.etoileDecorationPourMaster();
+            System.out.println("Votre proposition doit comporter " + longueurDelaCombinaison + " chiffres.");
+            Utils.etoileDecorationPourMaster();
+            System.out.println("SAISISSEZ VOTRE CHIFFRE MYSTERE : ");
+            nbSecretUtilisateur = nb.next();
+            break;
+        }
+        if (nbSecretUtilisateur.length() == tabNbSecretCpu.length) {
 
             Utils.etoileDecorationPourMaster();
-            System.out.print("saisisez votre proposition : ");
-            String essaiUtilisateur = nb.next();
-            Utils.etoileDecorationPourMaster();
-            int[] tabEssaiUtilisateur = new int[longueurDelaCombinaison];
-            for (int k = 0; k < tabEssaiUtilisateur.length; k++) {
-                int convEassaiUtilisateur = Integer.parseInt(essaiUtilisateur.charAt(k) + "");
-                tabEssaiUtilisateur[k] = convEassaiUtilisateur;
-            }
-
-            Random essaiCpu = new Random();
-            int[] tabEssaiCpu = new int[longueurDelaCombinaison];
-            for (int l = 0; l < tabEssaiCpu.length; l++) {
-                tabEssaiCpu[l] = essaiCpu.nextInt(9);
-            }
-            Utils.algoComportementRandom(tabEssaiCpu, tabEssaiUtilisateur);
-
-            System.out.println();
-            Utils.etoileDecorationPourMaster();
-            System.out.print(Arrays.toString(tabEssaiUtilisateur) + " Réponse pour joueur : ");
-            Utils.algoMaster(tabEssaiUtilisateur, tabNbSecretCpu);
-            Utils.etoileDecorationPourMaster();
-
-            System.out.println();
-            Utils.etoileDecorationPourMaster();
-            System.out.print(Arrays.toString(tabEssaiCpu) + " Réponse pour ordinateur : ");
-            Utils.algoMaster(tabEssaiCpu, tabNbSecretUtil);
+            System.out.println("Votre code secret est : " + Arrays.toString(tabNbSecretUtil));
             Utils.etoileDecorationPourMaster();
             System.out.println();
 
-            if (i == 1) {
+            for (int i = nbEssai; i >= 0; i--) {
 
-                System.out.println();
                 Utils.etoileDecorationPourMaster();
-                System.out.println("Attention dernier essai");
+                System.out.print("saisisez votre proposition : ");
+                String essaiUtilisateur = nb.next();
                 Utils.etoileDecorationPourMaster();
-                System.out.println();
-            }
 
-            if (i == 0) {
+                /*tant que la saisiUtilisateur n'est pas un nombre entre 0 et 9
+                envoie un message d'erreur*/
+                isUnNombre = essaiUtilisateur.matches("[0-9]*");
 
+                while (!isUnNombre) {
+                    Utils.etoileDecorationPourMaster();
+                    System.out.println("Vous n'avez pas saisi un nombre !");
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print("saisisez votre proposition : ");
+                    essaiUtilisateur = nb.next();
+                    isUnNombre = nbSecretUtilisateur.matches("[0-9]*");
+                    i++;
+                }
+
+                int[] tabEssaiUtilisateur = new int[longueurDelaCombinaison];
+                for (int k = 0; k < tabEssaiUtilisateur.length; k++) {
+                    int convEassaiUtilisateur = Integer.parseInt(essaiUtilisateur.charAt(k) + "");
+                    tabEssaiUtilisateur[k] = convEassaiUtilisateur;
+                }
+
+                /*tant que la proposition n'est pas de la bonne taille envoie ce message d'erreur,
+                y++ fait en sorte que chaque mauvaise saisie n'est pas compté comme un essai*/
+                while (essaiUtilisateur.length() < tabNbSecretCpu.length || essaiUtilisateur.length() > tabNbSecretCpu.length) {
+
+                    Utils.etoileDecorationPourMaster();
+                    System.out.println("Votre proposition doit comporter " + longueurDelaCombinaison + " chiffres.");
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print("saisisez votre proposition : ");
+                    essaiUtilisateur = nb.next();
+                    i++;
+                }
+
+                if (essaiUtilisateur.length() == tabNbSecretCpu.length) {
+
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print("saisisez votre proposition : ");
+                    essaiUtilisateur = nb.next();
+                    Utils.etoileDecorationPourMaster();
+
+                    /*tant que la saisiUtilisateur n'est pas un nombre entre 0 et 9
+                    envoie un message d'erreur*/
+                    isUnNombre = essaiUtilisateur.matches("[0-9]*");
+
+                    while (!isUnNombre) {
+                        Utils.etoileDecorationPourMaster();
+                        System.out.println("Vous n'avez pas saisi un nombre !");
+                        Utils.etoileDecorationPourMaster();
+                        System.out.print("saisisez votre proposition : ");
+                        essaiUtilisateur = nb.next();
+                        isUnNombre = nbSecretUtilisateur.matches("[0-9]*");
+                        i++;
+                    }
+
+                    tabEssaiUtilisateur = new int[longueurDelaCombinaison];
+
+                    for (int k = 0; k < tabEssaiUtilisateur.length; k++) {
+                        int convEassaiUtilisateur = Integer.parseInt(essaiUtilisateur.charAt(k) + "");
+                        tabEssaiUtilisateur[k] = convEassaiUtilisateur;
+                    }
+
+                    /*tant que la proposition n'est pas de la bonne taille envoie ce message d'erreur,
+                    y++ fait en sorte que chaque mauvaise saisie n'est pas compté comme un essai*/
+                    while (essaiUtilisateur.length() < tabNbSecretCpu.length || essaiUtilisateur.length() > tabNbSecretCpu.length) {
+
+                        Utils.etoileDecorationPourMaster();
+                        System.out.println("Votre proposition doit comporter " + longueurDelaCombinaison + " chiffres.");
+                        Utils.etoileDecorationPourMaster();
+                        System.out.print("saisisez votre proposition : ");
+                        essaiUtilisateur = nb.next();
+                        i++;
+                    }
+
+                    Random essaiCpu = new Random();
+                    int[] tabEssaiCpu = new int[longueurDelaCombinaison];
+                    for (int l = 0; l < tabEssaiCpu.length; l++) {
+                        tabEssaiCpu[l] = essaiCpu.nextInt(9 + 1);
+                    }
+
+                    Utils.algoComportementRandom(tabEssaiCpu, tabEssaiUtilisateur);
+
+                    System.out.println();
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print(Arrays.toString(tabEssaiUtilisateur) + " Réponse pour joueur : ");
+                    Utils.algoMaster(tabEssaiUtilisateur, tabNbSecretCpu);
+                    Utils.etoileDecorationPourMaster();
+
+                    System.out.println();
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print(Arrays.toString(tabEssaiCpu) + " Réponse pour ordinateur : ");
+                    Utils.algoMaster(tabEssaiCpu, tabNbSecretUtil);
+                    Utils.etoileDecorationPourMaster();
+                    System.out.println();
+
+                    if (i == 1) {
+
+                        System.out.println();
+                        Utils.etoileDecorationPourMaster();
+                        System.out.println("Attention dernier essai");
+                        Utils.etoileDecorationPourMaster();
+                        System.out.println();
+                    }
+
+                    if (i == 0) {
+
+                        Utils.etoileDecoration();
+                        Utils.hastagDecoration();
+                        System.out.println("                     PERDU !");
+                        Utils.hastagDecoration();
+                        Utils.etoileDecoration();
+                        System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
+                        System.out.println("Le code secret du joueur était : " + Arrays.toString(tabNbSecretUtil));
+                        break;
+                    }
+                    if (Arrays.equals(tabEssaiCpu, tabNbSecretUtil)) {
+                        Utils.etoileDecoration();
+                        Utils.hastagDecoration();
+                        System.out.println("             L'ordinateur a GAGNé !");
+                        Utils.hastagDecoration();
+                        Utils.etoileDecoration();
+                        System.out.println("L'ordinateur a trouvé votre code secret");
+                        System.out.println("La combinaison été : " + Arrays.toString(tabEssaiUtilisateur));
+                        break;
+                    }
+                    if (Arrays.equals(tabEssaiUtilisateur, tabNbSecretCpu)) {
+                        Utils.etoileDecoration();
+                        Utils.hastagDecoration();
+                        System.out.println("                    GAGNé !");
+                        Utils.hastagDecoration();
+                        Utils.etoileDecoration();
+                        System.out.println("Vous avez trouver le code secret de l'ordinateur !");
+                        System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
+                    }
+                }
                 Utils.etoileDecoration();
-                Utils.hastagDecoration();
-                System.out.println("                     PERDU !");
-                Utils.hastagDecoration();
-                Utils.etoileDecoration();
-                System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
-                System.out.println("Le code secret du joueur était : " + Arrays.toString(tabNbSecretUtil));
-                break;
-            }
-            if (Arrays.equals(tabEssaiCpu, tabNbSecretUtil)) {
-                Utils.etoileDecoration();
-                Utils.hastagDecoration();
-                System.out.println("             L'ordinateur a GAGNé !");
-                Utils.hastagDecoration();
-                Utils.etoileDecoration();
-                System.out.println("L'ordinateur a trouvé votre code secret");
-                System.out.println("La combinaison été : " + Arrays.toString(tabEssaiUtilisateur));
-                break;
-            }
-            if (Arrays.equals(tabEssaiUtilisateur, tabNbSecretCpu)) {
-                Utils.etoileDecoration();
-                Utils.hastagDecoration();
-                System.out.println("                    GAGNé !");
-                Utils.hastagDecoration();
-                Utils.etoileDecoration();
-                System.out.println("Vous avez trouver le code secret de l'ordinateur !");
-                System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
+                Menu.menuFinMaster();
             }
         }
-        Utils.etoileDecoration();
-        Menu.menuFinMaster();
     }
 }
-
--- src/com/company/DuelRecherche.java
+++ src/com/company/DuelRecherche.java
@@ -16,91 +16,191 @@ public class DuelRecherche {
         for (int i = 0; i < tabNbSecretCpu.length; i++) {
             tabNbSecretCpu[i] = nbAleatoire.nextInt(9);
         }
-        System.out.print(Arrays.toString(tabNbSecretCpu));
-        System.out.println();
 
         System.out.println("SAISISSEZ VOTRE CHIFFRE MYSTERE : ");
         String nbSecretUtilisateur = nb.next();
+
+                /*tant que la saisiUtilisateur n'est pas un nombre entre 0 et 9
+            envoie un message d'erreur
+             */
+        boolean isUnNombre = nbSecretUtilisateur.matches("[0-9]*");
+
+        while (!isUnNombre) {
+            Utils.etoileDecoration();
+            System.out.println("Vous n'avez pas saisi un nombre !");
+            Utils.etoileDecoration();
+            System.out.println("SAISISSEZ VOTRE CHIFFRE MYSTERE : ");
+            nbSecretUtilisateur = nb.next();
+            isUnNombre = nbSecretUtilisateur.matches("[0-9]*");
+        }
+
         int[] tabNbSecretUtil = new int[longueurDelaCombinaison];
         for (int j = 0; j < tabNbSecretUtil.length; j++) {
             int converter = Integer.parseInt(nbSecretUtilisateur.charAt(j) + "");
             tabNbSecretUtil[j] = converter;
         }
-        System.out.println(Arrays.toString(tabNbSecretUtil));
 
-        for (int i = nbEssai; i >= 0; i--) {
+        /*tant que la proposition n'est pas de la bonne taille envoie ce message d'erreur,
+          y++ fait en sorte que chaque mauvaise saisie n'est pas compté comme un essai*/
+        while (nbSecretUtilisateur.length() < tabNbSecretCpu.length || nbSecretUtilisateur.length() > tabNbSecretCpu.length) {
+            Utils.etoileDecoration();
+            System.out.println("Votre proposition doit comporter " + longueurDelaCombinaison + " chiffres.");
+            Utils.etoileDecoration();
+            System.out.println("SAISISSEZ VOTRE CHIFFRE MYSTERE : ");
+            nbSecretUtilisateur = nb.next();
+            break;
+        }
+        if (nbSecretUtilisateur.length() == tabNbSecretCpu.length) {
 
-            String essaiUtilisateur = nb.next();
-            int[] tabEssaiUtilisateur = new int[longueurDelaCombinaison];
-            for (int k = 0; k < tabEssaiUtilisateur.length; k++) {
-                int convEassaiUtilisateur = Integer.parseInt(essaiUtilisateur.charAt(k) + "");
-                tabEssaiUtilisateur[k] = convEassaiUtilisateur;
-            }
-
-            Random essaiCpu = new Random();
-            int[] tabEssaiCpu = new int[longueurDelaCombinaison];
-            for (int l = 0; l < tabEssaiCpu.length; l++) {
-                tabEssaiCpu[l] = essaiCpu.nextInt(9);
-            }
-
-            Utils.algoComportementRandom(tabEssaiCpu, tabEssaiUtilisateur);
-
-            System.out.println();
-            Utils.etoileDecorationPourMaster();
-            System.out.print(Arrays.toString(tabEssaiUtilisateur) + " Réponse pour joueur : ");
-            Utils.algoMaster(tabEssaiUtilisateur, tabNbSecretCpu);
-            Utils.etoileDecorationPourMaster();
-
-            System.out.println();
-            Utils.etoileDecorationPourMaster();
-            System.out.print(Arrays.toString(tabEssaiCpu) + " Réponse pour ordinateur : ");
-            Utils.algoMaster(tabEssaiCpu, tabNbSecretUtil);
-            Utils.etoileDecorationPourMaster();
+            Utils.etoileDecoration();
+            System.out.println("Votre code secret est : " + Arrays.toString(tabNbSecretUtil));
+            Utils.etoileDecoration();
             System.out.println();
 
-            if (i == 1) {
-
-                System.out.println();
-                Utils.etoileDecoration();
-                System.out.println("Attention dernier essai");
-                Utils.etoileDecoration();
-                System.out.println();
-            }
-
-            if (i == 0) {
+            for (int i = nbEssai; i >= 0; i--) {
 
                 Utils.etoileDecoration();
-                Utils.hastagDecoration();
-                System.out.println("                     PERDU !");
-                Utils.hastagDecoration();
+                System.out.print("saisisez votre proposition : ");
+                String essaiUtilisateur = nb.next();
                 Utils.etoileDecoration();
-                System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
-                System.out.println("Le code secret du joueur était : " + Arrays.toString(tabNbSecretUtil));
-                break;
-            }
 
-            if (Arrays.equals(tabEssaiCpu, tabNbSecretUtil)) {
-                Utils.etoileDecoration();
-                Utils.hastagDecoration();
-                System.out.println("             L'ordinateur a GAGNé !");
-                Utils.hastagDecoration();
-                Utils.etoileDecoration();
-                System.out.println("L'ordinateur a trouvé votre code secret");
-                System.out.println("La combinaison été : " + Arrays.toString(tabEssaiUtilisateur));
-                break;
-            }
+                /*tant que la saisiUtilisateur n'est pas un nombre entre 0 et 9
+                envoie un message d'erreur */
+                isUnNombre = essaiUtilisateur.matches("[0-9]*");
 
-            if (Arrays.equals(tabEssaiUtilisateur, tabNbSecretCpu)) {
+                while (!isUnNombre) {
+                    Utils.etoileDecoration();
+                    System.out.println("Vous n'avez pas saisi un nombre !");
+                    Utils.etoileDecoration();
+                    System.out.print("saisisez votre proposition : ");
+                    essaiUtilisateur = nb.next();
+                    isUnNombre = essaiUtilisateur.matches("[0-9]*");
+                    i++;
+                }
+
+                int[] tabEssaiUtilisateur = new int[longueurDelaCombinaison];
+                for (int k = 0; k < tabEssaiUtilisateur.length; k++) {
+                    int convEassaiUtilisateur = Integer.parseInt(essaiUtilisateur.charAt(k) + "");
+                    tabEssaiUtilisateur[k] = convEassaiUtilisateur;
+                }
+
+                /*tant que la proposition n'est pas de la bonne taille envoie ce message d'erreur,
+                y++ fait en sorte que chaque mauvaise saisie n'est pas compté comme un essai*/
+                while (essaiUtilisateur.length() < tabNbSecretCpu.length || essaiUtilisateur.length() > tabNbSecretCpu.length) {
+
+                    Utils.etoileDecoration();
+                    System.out.println("Votre proposition doit comporter " + longueurDelaCombinaison + " chiffres.");
+                    Utils.etoileDecoration();
+                    System.out.print("saisisez votre proposition : ");
+                    essaiUtilisateur = nb.next();
+                    i++;
+                }
+                if (essaiUtilisateur.length() == tabNbSecretCpu.length) {
+
+                    Utils.etoileDecoration();
+                    System.out.print("saisisez votre proposition : ");
+                    essaiUtilisateur = nb.next();
+                    Utils.etoileDecoration();
+
+                      /*tant que la saisiUtilisateur n'est pas un nombre entre 0 et 9
+                      envoie un message d'erreur */
+
+                    isUnNombre = essaiUtilisateur.matches("[0-9]*");
+
+                    while (!isUnNombre) {
+                        Utils.etoileDecoration();
+                        System.out.println("Vous n'avez pas saisi un nombre !");
+                        Utils.etoileDecoration();
+                        System.out.print("saisisez votre proposition : ");
+                        essaiUtilisateur = nb.next();
+                        isUnNombre = essaiUtilisateur.matches("[0-9]*");
+                        i++;
+                    }
+
+                    tabEssaiUtilisateur = new int[longueurDelaCombinaison];
+                    for (int k = 0; k < tabEssaiUtilisateur.length; k++) {
+                        int convEassaiUtilisateur = Integer.parseInt(essaiUtilisateur.charAt(k) + "");
+                        tabEssaiUtilisateur[k] = convEassaiUtilisateur;
+                    }
+
+                    /*tant que la proposition n'est pas de la bonne taille envoie ce message d'erreur,
+                    y++ fait en sorte que chaque mauvaise saisie n'est pas compté comme un essai*/
+                    while (essaiUtilisateur.length() < tabNbSecretCpu.length || essaiUtilisateur.length() > tabNbSecretCpu.length) {
+
+                        Utils.etoileDecoration();
+                        System.out.println("Votre proposition doit comporter " + longueurDelaCombinaison + " chiffres.");
+                        Utils.etoileDecoration();
+                        System.out.print("saisisez votre proposition : ");
+                        essaiUtilisateur = nb.next();
+                        i++;
+                    }
+
+                    Random essaiCpu = new Random();
+                    int[] tabEssaiCpu = new int[longueurDelaCombinaison];
+                    for (int l = 0; l < tabEssaiCpu.length; l++) {
+                        tabEssaiCpu[l] = essaiCpu.nextInt(9 + 1);
+                    }
+
+                    Utils.algoComportementRandom(tabEssaiCpu, tabEssaiUtilisateur);
+
+                    System.out.println();
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print(Arrays.toString(tabEssaiUtilisateur) + " Réponse pour joueur : ");
+                    Utils.algoMaster(tabEssaiUtilisateur, tabNbSecretCpu);
+                    Utils.etoileDecorationPourMaster();
+
+                    System.out.println();
+                    Utils.etoileDecorationPourMaster();
+                    System.out.print(Arrays.toString(tabEssaiCpu) + " Réponse pour ordinateur : ");
+                    Utils.algoMaster(tabEssaiCpu, tabNbSecretUtil);
+                    Utils.etoileDecorationPourMaster();
+                    System.out.println();
+
+                    if (i == 1) {
+
+                        System.out.println();
+                        Utils.etoileDecoration();
+                        System.out.println("Attention dernier essai");
+                        Utils.etoileDecoration();
+                        System.out.println();
+                    }
+
+                    if (i == 0) {
+
+                        Utils.etoileDecoration();
+                        Utils.hastagDecoration();
+                        System.out.println("                     PERDU !");
+                        Utils.hastagDecoration();
+                        Utils.etoileDecoration();
+                        System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
+                        System.out.println("Le code secret du joueur était : " + Arrays.toString(tabNbSecretUtil));
+                        break;
+                    }
+
+                    if (Arrays.equals(tabEssaiCpu, tabNbSecretUtil)) {
+                        Utils.etoileDecoration();
+                        Utils.hastagDecoration();
+                        System.out.println("             L'ordinateur a GAGNé !");
+                        Utils.hastagDecoration();
+                        Utils.etoileDecoration();
+                        System.out.println("L'ordinateur a trouvé votre code secret");
+                        System.out.println("La combinaison été : " + Arrays.toString(tabEssaiUtilisateur));
+                        break;
+                    }
+
+                    if (Arrays.equals(tabEssaiUtilisateur, tabNbSecretCpu)) {
+                        Utils.etoileDecoration();
+                        Utils.hastagDecoration();
+                        System.out.println("                    GAGNé !");
+                        Utils.hastagDecoration();
+                        Utils.etoileDecoration();
+                        System.out.println("Vous avez trouver le code secret de l'ordinateur !");
+                        System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
+                    }
+                }
                 Utils.etoileDecoration();
-                Utils.hastagDecoration();
-                System.out.println("                    GAGNé !");
-                Utils.hastagDecoration();
-                Utils.etoileDecoration();
-                System.out.println("Vous avez trouver le code secret de l'ordinateur !");
-                System.out.println("Le code secret de l'ordinateur était : " + Arrays.toString(tabNbSecretCpu));
+                Menu.menuFinRecherchePlusMoins();
             }
         }
-        Utils.etoileDecoration();
-        Menu.menuFinRecherchePlusMoins();
     }
 }
